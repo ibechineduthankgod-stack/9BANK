3295c9a512fe1d98b98abba24abb16c3817eff3f
ibechineduthankgod-stack <ibechineduthankgod@gmail.com>
2026-03-13 11:16:41 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -86,7 +86,7 @@ if(amount===""){
 message.innerText="Enter an amount"
 
 return
-
+updateChart(amount);
 }
 
 message.innerText="Transfer Successful"
@@ -96,28 +96,108 @@ addTransaction(amount)
 updateBalance(amount)
 
 }
-let balance=24850
+let balance = 24850;
 
-function updateBalance(amount){
+const sendBtn = document.getElementById("sendBtn");
 
-balance=balance-amount
+sendBtn.addEventListener("click", function(){
 
-document.querySelector(".balance-card h1").innerText="$"+balance
+let account = document.getElementById("account").value;
+
+let amount = document.getElementById("amount").value;
+
+let message = document.getElementById("transferMessage");
+
+if(account === "" || amount === ""){
+
+message.innerText = "Please enter account and amount";
+return;
 
 }
-function addTransaction(amount){
 
-let table=document.querySelector(".transactions table")
+amount = parseFloat(amount);
 
-let row=table.insertRow(-1)
+if(amount > balance){
 
-row.innerHTML=`
+message.innerText = "Insufficient Balance";
+return;
 
+}
+
+balance -= amount;
+
+document.getElementById("balance").innerText = "$" + balance;
+
+message.innerText = "Transfer Successful";
+
+addTransaction(account, amount);
+
+showNotification(amount);
+
+});
+
+function addTransaction(account, amount){
+
+let table = document.getElementById("transactionTable");
+
+let row = table.insertRow(-1);
+
+row.innerHTML = `
 <td>Today</td>
-<td>Transfer</td>
+<td>Transfer to ${account}</td>
 <td>-$${amount}</td>
 <td>Completed</td>
-
-`
+`;
 
 }
+
+const ctx = document.getElementById("financeChart");
+
+let chartData = [2000,3500,4200,3800,5200,6100];
+
+const financeChart = new Chart(ctx,{
+
+type:"line",
+
+data:{
+labels:["Jan","Feb","Mar","Apr","May","Jun"],
+datasets:[{
+label:"Account Balance",
+data:chartData,
+borderWidth:3
+}]
+},
+
+options:{responsive:true}
+
+});
+
+function updateChart(amount){
+
+chartData.push(balance);
+
+financeChart.update();
+
+}
+function showNotification(amount){
+
+let note = document.createElement("div");
+
+note.className = "notification";
+
+note.innerText = "$" + amount + " Transfer Completed";
+
+document.body.appendChild(note);
+
+setTimeout(()=>{
+note.remove();
+},4000);
+
+}
+const toggle=document.getElementById("darkToggle");
+
+toggle.addEventListener("click",()=>{
+
+document.body.classList.toggle("dark-mode");
+
+});
